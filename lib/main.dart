8d026411a566f78747cdac9509892d4ed62e60ba
Manuel Kpatsu <manuelkpatsu@gmail.com>
2021-06-11 08:55:42 +0000

    Add bottom tabs and a drawer to home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'screens/filters_screen.dart';
+import 'screens/tabs_screen.dart';
 import 'screens/meal_detail_screen.dart';
 import 'screens/category_meals_screen.dart';
 import 'screens/categories_screen.dart';
@@ -31,11 +33,12 @@ class MyApp extends StatelessWidget {
               ),
             ),
       ),
-      initialRoute: CategoriesScreen.routeName,
+      initialRoute: '/',
       routes: {
-        CategoriesScreen.routeName: (context) => CategoriesScreen(),
+        '/': (context) => TabsScreen(),
         CategoryMealsScreen.routeName: (context) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (context) => MealDetailScreen(),
+        FiltersScreen.routeName: (context) => FiltersScreen(),
       },
       onUnknownRoute: (settings) {
         return MaterialPageRoute(builder: (context) => CategoriesScreen());
--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -8,27 +8,22 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('The Meal'), // so it does not rebuild since it is not going to change
-      ),
-      body: GridView(
-        padding: EdgeInsets.all(20),
-        children: DUMMY_CATEGORIES
-            .map(
-              (categoryData) => CategoryItem(
-                categoryData.id,
-                categoryData.title,
-                categoryData.color,
-              ),
-            )
-            .toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
+    return GridView(
+      padding: EdgeInsets.all(20),
+      children: DUMMY_CATEGORIES
+          .map(
+            (categoryData) => CategoryItem(
+              categoryData.id,
+              categoryData.title,
+              categoryData.color,
+            ),
+          )
+          .toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
     );
   }
